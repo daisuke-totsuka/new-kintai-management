a4e32eddb525fc753c2827b31db0063aa7166e8c
Daisuke Totsuka <totsuka.daisuke@nisz.co.jp>
2026-03-28 13:22:25 +0900

    Implement attendance input feature

--- backend/api/login/login.py
+++ backend/api/login/login.py
@@ -5,26 +5,32 @@ from flask_cors import CORS
 
 # Blueprint作成
 login_bp = Blueprint("login", __name__)
-CORS(login_bp)
+#CORS(login_bp)
 
 # Service生成
 service = AuthService()
 
 
 #@login_bp.route("/login", methods=["POST"])
-@login_bp.route("/login", methods=["POST", "OPTIONS"])
+#@login_bp.route("/login", methods=["POST", "OPTIONS"])
+@login_bp.route("/login", methods=["POST"])
 def login():
     print("logi nAPI called")
     
-    if request.method == "OPTIONS":
-        return jsonify({"message": "ok"}), 200
+    #if request.method == "OPTIONS":
+        #return jsonify({"message": "ok"}), 200
         #data = request.json
  
     try:
-       data = request.get_json(silent=True)
+       #data = request.get_json(silent=True)
        
-       if not data:
-            return jsonify({"error": "Invalid JSON"}), 400
+       #if not data:
+            #return jsonify({"error": "Invalid JSON"}), 400
+       
+       data = request.get_json()
+
+       if data is None:
+          return jsonify({"error": "Invalid JSON"}), 400
         
        email    = data.get("email")
        password = data.get("password")
